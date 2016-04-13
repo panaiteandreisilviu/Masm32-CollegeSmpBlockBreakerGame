092765c015837204876446dae9179d0c10c622c6
panaiteandreisilviu <andrei_silviu.panaite@aut.pub.ro>
2016-04-14 01:27:14 +0300

    added blocks generation

--- ProiectSMP.asm
+++ ProiectSMP.asm
@@ -10,11 +10,16 @@ include \masm32\include\kernel32.inc
 includelib \masm32\lib\user32.lib
 includelib \masm32\lib\kernel32.lib
 include \masm32\include\masm32rt.inc
+include blocks.asm
 
 .data
 ClassName db "Proiect SMP",0
 AppName  db "Proiect SMP",0
 OurText  db "Sisteme cu MicroProcesoare",0
+
+MsgBoxCaption  db "Game Over",0
+MsgBoxText       db "Press OK to restart.",0
+
 
 WM_FINISH equ WM_USER+100h
 
@@ -24,6 +29,9 @@ Solid db "Solid Pen",0
 brush db "Solid brush",0
 testString db "Sisteme cu MicroProcesoare",0
 
+playerString db "Player :  ",0
+
+pointsString db "Points  ",0
 
 racket_x1 dd 200
 racket_y1 dd 435
@@ -35,13 +43,25 @@ ball_x2 dd 260
 ball_y1 dd 400
 ball_y2 dd 385
 
+blocks_row1 dd  1,1,1,1,1,1,1,1,1
+blocks_row2 dd  1,1,1,1,1,1,1,1,1
+blocks_row3 dd  1,1,1,1,1,1,1,1,1
+
 ball_speed_x dd 5
 ball_speed_y dd 5
-
 
 acceleration dd 10
 
 char WPARAM 9
+
+blockOffset dd 3
+
+rowOffset dd 20
+row1BottomX dd 90
+row2BottomX dd 120
+row3BottomX dd 150
+
+
 
 mousePos POINT <>
 .data?
@@ -49,6 +69,10 @@ hInstance HINSTANCE ?
 CommandLine LPSTR ?
 hwnd HANDLE ?
 ThreadID DWORD ?
+
+;hOldSolidbrush DWORD ? ;might fix memory leak?
+;hOldpen:DWORD
+
 .code
 start:
 	invoke GetModuleHandle, NULL
@@ -92,7 +116,7 @@ WinMain proc hInst:HINSTANCE,hPrevInst:HINSTANCE,CmdLine:LPSTR,CmdShow:DWORD
 	invoke RegisterClassEx, addr wc
 	INVOKE CreateWindowEx,NULL,ADDR ClassName,ADDR AppName,\
            WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,\
-           CW_USEDEFAULT,500,500,NULL,NULL,\
+           CW_USEDEFAULT,472,500,NULL,NULL,\
            hInst,NULL
 	mov   hwnd,eax
 	INVOKE ShowWindow, hwnd,SW_SHOWNORMAL
@@ -111,6 +135,8 @@ WinMain endp
 ;____________________________________________________
 
 
+
+
 		
 		
 WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
@@ -124,7 +150,7 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
 	local hSolidbrush:DWORD
 	local hOldSolidbrush:DWORD
 	local hRedSolidbrush:DWORD
-	local hBlackSolidbrush:DWORD
+	local hGraySolidbrush:DWORD
 	
 	
 
@@ -134,7 +160,7 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
 		RGB 100,100,00
 		invoke SetBkColor,eax,White
 		invoke GetStockObject,WHITE_BRUSH
-		ret
+	ret
 		
 	.ELSEIF uMsg==WM_CHAR
         
@@ -155,9 +181,15 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
 			mov eax,acceleration
 			add racket_x1, eax
 		.elseif wParam=="q" || wParam=="Q"
-			dec acceleration
+			;mov edi, OFFSET blocks_row1
+			;mov eax, [edi]     ; get number from warray			
+			;mov racket_x1,eax;
+			;dec acceleration
 		.elseif wParam=="e" || wParam=="E"
-			inc acceleration
+			;mov edi, OFFSET blocks_row1
+			;mov eax, [edi + 4]     ; get number from warray			
+			;mov racket_x1,eax;
+			;inc acceleration
 		.endif
 		
 		mov eax,racket_x1
@@ -194,42 +226,71 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
 		
 		;FIX ME CLEAR BRUSHES AFTER END PAINT
 		
-		;______________Create Brushes_________________
+		;______________Create Brushes____________
 		RGB 50,50,50
 		invoke CreatePen,PS_SOLID,1,eax	;create our pen
 		mov hPen,eax
 		
-		invoke CreateHatchBrush,HS_BDIAGONAL,Red	; our hatch brush
-		mov hBrush,eax
+		;invoke CreateHatchBrush,HS_BDIAGONAL,Red	; our hatch brush
+		;mov hBrush,eax
 		
 		RGB 120,120,120
 		invoke CreateSolidBrush,eax	;our solid brush
-		mov hSolidbrush,eax	
+		mov hSolidbrush,eax
 		
 		
 		invoke CreateSolidBrush,Red	;our solid brush
 		mov hRedSolidbrush,eax	
-		;____________________________________________
+		;________________________________________
 		
 		
-		;______________Clear Screen__________________
+		
+		;______________Clear Screen______________
 		RGB 170,170,170
 		invoke CreateSolidBrush,eax	;our solid brush
-		mov hBlackSolidbrush,eax
+		mov hGraySolidbrush,eax
 		
-		invoke SelectObject,hdc,hBlackSolidbrush
+		invoke SelectObject,hdc,hGraySolidbrush
 		mov hOldSolidbrush,eax
-		invoke Rectangle,hdc,-10,-10,1500,1500
+		invoke Rectangle,hdc,-10,-10,700,700
 		invoke SelectObject,hdc,hOldSolidbrush
-		;___________________________________________
+		;________________________________________
+		
+		
+		;______________Header____________________
+		RGB 90,90,90
+		invoke CreateSolidBrush,eax	;our solid brush
+		mov hGraySolidbrush,eax
+		
+		invoke SelectObject,hdc,hGraySolidbrush
+		mov hOldSolidbrush,eax
+		invoke Rectangle,hdc,0,0,470,40
+		invoke SelectObject,hdc,hOldSolidbrush
+		;________________________________________
+		
+		RGB    230,230,230
+        invoke SetTextColor,hdc,eax
+        RGB    90,90,90
+        invoke SetBkColor,hdc,eax
+		invoke TextOut,hdc,10,15,addr playerString,sizeof playerString-1
+		
+		invoke TextOut,hdc,350,15,addr pointsString,sizeof pointsString-1
+		
+
+		
 		
 	
-		;___________Draw racket____________________
+		;___________Draw racket__________________
 		invoke SelectObject,hdc,hSolidbrush
 		mov hOldSolidbrush,eax
 		invoke Rectangle,hdc,racket_x1,racket_y1,racket_x2,racket_y2
 		invoke SelectObject,hdc,hOldSolidbrush
-		;__________________________________________
+		;________________________________________
+		
+		
+		;__________Draw Blocks___________________
+		DrawBlocks
+		;________________________________________
 		
 		
 		;___________Draw Ball____________________
@@ -237,14 +298,13 @@ WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
 		mov hOldSolidbrush,eax
 		invoke Ellipse,hdc,ball_x1,ball_y1,ball_x2,ball_y2
 		invoke SelectObject,hdc,hOldSolidbrush
-		;__________________________________________
+		;________________________________________
 		
 		
 		;invoke SelectObject,hdc,hPen
 		;mov eax,hOldpen
 		;invoke Ellipse,hdc,110,110,210,210
 		;invoke SelectObject,hdc,hOldpen
-		
 		
 		
 		;invoke TextOut,hdc,racket_x1,racket_y1,ADDR acceleration,sizeof acceleration-1
@@ -289,46 +349,101 @@ GameLoop:
     sub ball_y2,eax;
     
     
+    ;__________Change Ball Direction_______
     cmp ball_x1,0
 	jbe changeXDir
 	
-	cmp ball_x1,475
+	cmp ball_x1,446
 	jae changeXDir
     
-    cmp ball_y1,10
-	jbe changeYDir  
+    cmp ball_y1,50
+	jbe changeYTopDir  
     
     cmp ball_y1,440
-	jae changeYDir
-	
+	jae changeYBottomDir
+    ;_____________________________________
+    
+    ;_______Check for ball racket collision
+    
+    ;_____________________________________	
     invoke Sleep, 16 ;60FPS
     invoke InvalidateRect, hwnd,NULL,FALSE
 	invoke  UpdateWindow,hwnd
 	
-jmp  GameLoop
+LOOP GameLoop
+
         
-changeYDir:
+changeYTopDir:
+	;change y movement
 	mov ebx,-1
 	mov eax,ball_speed_y
 	mul ebx
 	mov ball_speed_y,eax
-	jmp  GameLoop
+jmp  GameLoop
+	
 
-changeXDir:
+changeYBottomDir:
+	mov ecx,racket_x1
+	mov edx,racket_x2
+	sub ecx,10
+	add edx,10
+	
+	;check if ball is outside of racket area
+	;cmp ball_x1,ecx
+	;jbe gameOver
+	
+	;cmp ball_x1,edx
+	;jae gameOver
+	
+	
+	;change y movement
+	mov ebx,-1
+	mov eax,ball_speed_y
+	mul ebx
+	mov ball_speed_y,eax
+jmp  GameLoop
+
+changeXDir: 
 	mov ebx,-1
 	mov eax,ball_speed_x
 	mul ebx
 	mov ball_speed_x,eax
-	jmp  GameLoop
-		
+jmp  GameLoop
+
+
+
+gameOver:
+	invoke MessageBox, NULL, addr MsgBoxText, addr MsgBoxCaption, MB_OK
+	jmp resetGame
+LOOP gameOver
+
+resetGame:
+
+	mov racket_x1,200
+	mov racket_y1,435
+	mov racket_x2,300
+	mov racket_y2,445
+	
+	mov ball_x1,245
+	mov ball_x2,260
+	mov ball_y1,400
+	mov ball_y2,385
+	
+	mov ball_speed_x,5
+	mov ball_speed_y,5
+	
+	
+	mov acceleration,10
+
+	jmp GameLoop
+LOOP resetGame
 	
 Get_out: 
         invoke PostMessage,hwnd,WM_FINISH,NULL,NULL
-        ret 
+        ret
 GameThread ENDP
 
 ;______________________________________
-
 
 
 
---
+++ blocks.asm
@@ -0,0 +1,113 @@
+DrawBlocks macro
+	
+	invoke SelectObject,hdc,hSolidbrush
+	mov hOldSolidbrush,eax
+	
+	
+	mov esi,0 ;loop counter
+	mov edi,50 ;horizontal position offset
+	drawRow1:
+		
+		;check if block has been destroyed
+		mov ebx, OFFSET blocks_row1 ; base pointer
+		mov eax, [ebx + esi*4]      ; get value from array (ebx is base pointer, esi is offset)		
+		cmp eax,0
+		jz skipDraw1
+				
+		mov eax,esi
+		mul edi ;edi*esi
+		
+		add eax,blockOffset
+		mov ebx,eax; x1
+		add eax,edi
+		sub eax,blockOffset
+		mov ecx,eax; x2
+		
+		mov eax,row1BottomX; y2
+		mov edx ,row1BottomX
+		sub edx ,rowOffset ;y1
+		invoke Rectangle,hdc,ebx,edx,ecx,eax
+		
+		skipDraw1:
+		inc esi
+		cmp esi,8
+	jbe drawRow1
+	
+	
+	
+	mov esi,0 ;loop counter
+	mov edi,50 ;horizontal position offset
+	drawRow2:
+		
+		;mov edi, OFFSET bl		;check if block has been destroyed
+		mov ebx, OFFSET blocks_row2 ; base pointer
+		mov eax, [ebx + esi*4]      ; get value from array (ebx is base pointer, esi is offset)		
+		cmp eax,0
+		jz skipDraw2
+		
+		mov eax,esi
+		mul edi ;edi*esi
+		
+		add eax,blockOffset
+		mov ebx,eax; x1
+		add eax,edi
+		sub eax,blockOffset
+		mov ecx,eax; x2
+		
+		mov eax,row2BottomX; y2
+		mov edx ,row2BottomX
+		sub edx ,rowOffset ;y1
+		invoke Rectangle,hdc,ebx,edx,ecx,eax
+		
+		skipDraw2:
+		inc esi
+		cmp esi,8
+	jbe drawRow2
+	
+	
+	
+	mov esi,0 ;loop counter
+	mov edi,50 ;horizontal position offset
+	drawRow3:
+		
+		;mov edi, OFFSET bl		;check if block has been destroyed
+		mov ebx, OFFSET blocks_row3 ; base pointer
+		mov eax, [ebx + esi*4]      ; get value from array (ebx is base pointer, esi is offset)		
+		cmp eax,0
+		jz skipDraw3
+		
+		mov eax,esi
+		mul edi ;edi*esi
+		
+		add eax,blockOffset
+		mov ebx,eax; block.x1
+		add eax,edi
+		sub eax,blockOffset
+		mov ecx,eax; block.x2
+		
+		mov eax,row3BottomX; y2
+		mov edx ,row3BottomX
+		sub edx ,rowOffset ;y1
+		invoke Rectangle,hdc,ebx,edx,ecx,eax
+		
+		skipDraw3:
+		inc esi
+		cmp esi,8
+	jbe drawRow3
+	
+			
+
+	;invoke Rectangle,hdc,0,0,50,20
+	;invoke Rectangle,hdc,50,0,100,20
+	;invoke Rectangle,hdc,100,0,150,20
+	;invoke Rectangle,hdc,150,0,200,20
+	;invoke Rectangle,hdc,200,0,250,20
+	;invoke Rectangle,hdc,250,0,300,20
+	;invoke Rectangle,hdc,300,0,350,20
+	;invoke Rectangle,hdc,350,0,400,20
+	;invoke Rectangle,hdc,400,0,450,20
+	
+	
+	
+	invoke SelectObject,hdc,hOldSolidbrush
+endm
